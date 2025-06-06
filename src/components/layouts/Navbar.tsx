51170d7d7d164bf5b38ccf8d029b0ff417fe4aeb
syahfrinorezky <syahfrinorezky@gmail.com>
2025-06-06 19:21:40 +0800

    style(navbar): fix style error on navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -36,84 +36,86 @@ function Navbar() {
   };
 
   return (
-    <div className="flex items-center justify-between py-3 px-5 bg-white text-primary shadow-lg md:max-w-[1200px] md:mx-auto">
-      <div className="logo">
-        <Image
-          src={"/images/lofie.png"}
-          width={500}
-          height={500}
-          alt="Lofie Homemade"
-          className="w-12"
-        />
-      </div>
-      <div className="navigation">
-        <button type="button" className="md:hidden" onClick={toggleMenu}>
-          <HiBars3 size={25} />
-        </button>
+    <div className="bg-white text-primary shadow-lg">
+      <div className="flex items-center justify-between py-3 px-5 md:max-w-[1200px] md:mx-auto">
+        <div className="logo">
+          <Image
+            src={"/images/lofie.png"}
+            width={500}
+            height={500}
+            alt="Lofie Homemade"
+            className="w-12"
+          />
+        </div>
+        <div className="navigation">
+          <button type="button" className="md:hidden" onClick={toggleMenu}>
+            <HiBars3 size={25} />
+          </button>
 
-        {/* slide menu navbar for mobile - start */}
-        <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.2, ease: "easeInOut" }}
-              className={clsx(
-                "fixed inset-0 bg-black/30 backdrop-blur-xs z-10"
-              )}
-              onClick={toggleMenu}>
+          {/* slide menu navbar for mobile - start */}
+          <AnimatePresence>
+            {isOpen && (
               <motion.div
-                initial={{ x: "100%" }}
-                animate={{ x: 0 }}
-                exit={{ x: "100%" }}
-                transition={{ duration: 0.3, ease: "easeInOut" }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2, ease: "easeInOut" }}
                 className={clsx(
-                  "fixed top-0 bottom-0 right-0 w-3/5 bg-secondary text-black"
+                  "fixed inset-0 bg-black/30 backdrop-blur-xs z-10"
                 )}
-                onClick={(e) => e.stopPropagation()}>
-                <button
-                  type="button"
-                  onClick={toggleMenu}
-                  className="absolute right-5 top-2 p-3 rounded-md cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-300">
-                  <HiXMark size={25} />
-                </button>
-                <ul className="relative top-15 p-6 flex flex-col gap-2">
-                  {lofiemenu.map((item) => (
-                    <li key={item.id}>
-                      <Link
-                        href={item.link}
-                        className={clsx(
-                          "py-3 px-2 flex gap-2 items-center rounded-md w-full transition-colors duration-300 ease-in-out cursor-pointer hover:bg-gray-300",
-                          isActive(item.link) ? "bg-gray-300" : ""
-                        )}>
-                        <span className="text-primary">
-                          {item.icon && <item.icon size={20} />}
-                        </span>
-                        <span>{item.name}</span>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+                onClick={toggleMenu}>
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: 0 }}
+                  exit={{ x: "100%" }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className={clsx(
+                    "fixed top-0 bottom-0 right-0 w-3/5 bg-secondary text-black"
+                  )}
+                  onClick={(e) => e.stopPropagation()}>
+                  <button
+                    type="button"
+                    onClick={toggleMenu}
+                    className="absolute right-5 top-2 p-3 rounded-md cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-300">
+                    <HiXMark size={25} />
+                  </button>
+                  <ul className="relative top-15 p-6 flex flex-col gap-2">
+                    {lofiemenu.map((item) => (
+                      <li key={item.id}>
+                        <Link
+                          href={item.link}
+                          className={clsx(
+                            "py-3 px-2 flex gap-2 items-center rounded-md w-full transition-colors duration-300 ease-in-out cursor-pointer hover:bg-gray-300",
+                            isActive(item.link) ? "bg-gray-300" : ""
+                          )}>
+                          <span className="text-primary">
+                            {item.icon && <item.icon size={20} />}
+                          </span>
+                          <span>{item.name}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </motion.div>
               </motion.div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        {/* slide menu navbar for mobile - end */}
+            )}
+          </AnimatePresence>
+          {/* slide menu navbar for mobile - end */}
 
-        {/* navbar for desktop - start */}
-        <div className="hidden md:flex items-center gap-3">
-          {lofiemenu.map((item) => (
-            <Link
-              key={item.id}
-              href={item.link}
-              className={clsx(
-                "py-2 px-3 flex gap-2 items-center rounded-md w-full transition-colors duration-300 ease-in-out cursor-pointer hover:text-primary",
-                isActive(item.link) ? "text-primary" : "text-stone-800"
-              )}>
-              <span>{item.name}</span>
-            </Link>
-          ))}
+          {/* navbar for desktop - start */}
+          <div className="hidden md:flex items-center gap-3">
+            {lofiemenu.map((item) => (
+              <Link
+                key={item.id}
+                href={item.link}
+                className={clsx(
+                  "py-2 px-3 flex gap-2 items-center rounded-md w-full transition-colors duration-300 ease-in-out cursor-pointer hover:text-primary",
+                  isActive(item.link) ? "text-primary" : "text-stone-800"
+                )}>
+                <span>{item.name}</span>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
